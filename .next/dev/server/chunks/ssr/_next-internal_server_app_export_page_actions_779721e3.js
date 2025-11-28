544e29157b4af8a3c5656628c686f28da259bf2c
aahaanya <aahaanyacreatives@gmail.com>
2025-11-28 22:01:49 +0530

    first commit

--- .next/dev/server/chunks/ssr/_next-internal_server_app_export_page_actions_779721e3.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/export/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_export_page_actions_779721e3.js.map
